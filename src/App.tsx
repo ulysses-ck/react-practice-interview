22a5c0bb6ac668bd258fdbabf2a73d39849623ef
Ulises <ulisesapaza.280@gmail.com>
2025-12-14 09:37:34 -0300

    app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Kata1 } from "./components/kata-1";
+import { Kata2 } from "./components/kata-2";
+import { Kata3 } from "./components/kata-3";
+import { Kata4 } from "./components/kata-4";
+import { Kata5 } from "./components/kata-5";
+import { ThemeProvider } from "./providers/theme-provider";
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div>
+        {/* <Kata1 /> */}
+        {/* <Kata2 /> */}
+        {/* <Kata3 /> */}
+        {/* <Kata4 /> */}
+        <Kata5 />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
