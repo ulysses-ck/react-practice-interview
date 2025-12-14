37b78b45c4690b0b2563540f1373bdf86885dcad
Ulises <ulisesapaza.280@gmail.com>
2025-12-14 09:36:07 -0300

    kata 4

---
+++ src/components/kata-4.tsx
@@ -0,0 +1,16 @@
+import { useLocalStorage } from "../hooks/use-local-storage";
+
+export function Kata4() {
+  const { actualValue, setActualValue } = useLocalStorage("text-input", "");
+
+  return (
+    <div>
+      <label htmlFor="text-input">Text input</label>
+      <input
+        type="text"
+        value={actualValue}
+        onChange={(e) => setActualValue(e.currentTarget.value)}
+      />
+    </div>
+  );
+}
